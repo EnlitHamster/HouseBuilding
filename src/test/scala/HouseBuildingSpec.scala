76b307c52fb554131f8cf15476192af3f2767409
Sandro Massa <sandro2398@hotmail.it>
2020-03-13 02:55:58 +0100

    Added test file

---
+++ src/test/scala/HouseBuildingSpec.scala
@@ -0,0 +1,21 @@
+import Message.Operation._
+
+import akka.actor.{ActorSystem, Props}
+import akka.util.Timeout
+import akka.pattern.ask
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class HouseBuildingSpec extends AnyFlatSpec {
+  val System: ActorSystem = ActorSystem(s"System")
+
+  "A company" should "build a house" in {
+    implicit val Timeout: Timeout = 1 minute
+    val Future = System.actorOf(Props[ConstructionCompany], s"Company") ? BuildHouse
+    assert(Await.result(Future, Timeout.duration) === HouseBuilt)
+  }
+}
