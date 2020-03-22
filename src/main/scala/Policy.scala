651db3981b0d2c07cad251dadb11065d41604d4e
Sandro Massa <sandro2398@hotmail.it>
2020-03-23 00:54:24 +0100

    Basic Policy implementaiton

--- src/main/scala/Policy.scala
+++ src/main/scala/Policy.scala
@@ -1,3 +1,10 @@
 trait Policy {
+  // TODO: Add possibly exceptions registration system (like events)
+
+  // Does the policy applies to a certain set of conditions
+  def applies(conditions: Any*): Boolean
+  // Acts in response to a report
   def choose(report: Report): PartialFunction[Any, Unit]
+  // Acts in response to an exception
+  def handle(exception: Exception): PartialFunction[Any, Unit]
 }
