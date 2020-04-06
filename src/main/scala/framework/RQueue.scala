da88c3a666adafb991d6f4a601adc0366872dcbd
Sandro Massa <sandro2398@hotmail.it>
2020-04-06 15:10:51 +0200

    Added implementations & tests

--- src/main/scala/framework/RQueue.scala
+++ src/main/scala/framework/RQueue.scala
@@ -1,5 +1,34 @@
 package framework
 
-class RQueue {
+import scala.collection.mutable
+import Predicts._
 
+import scala.reflect.ClassTag
+
+class RQueue[T: ClassTag](val Size: Int) {
+  checkArg(Size >= 0, s"Size (%s) must >= 0", Size)
+  private val Delegate = mutable.Queue[T]()
+
+  def +(element: T): RQueue[T] = {
+    checkNotNull(element, s"Cannot add null element to RQueue")
+    if (Size == 0) return this
+    if (Delegate.length == Size) Delegate.dequeue
+    Delegate.enqueue(element)
+    this
+  }
+
+  def ++(elements: Array[T]): RQueue[T] = {
+    checkNotNull(elements, s"Cannot add null element to RQueue")
+    if (Size == 0) return this
+    val Delta: Int = Size - Delegate.length
+    val CollSize: Int = elements.length
+    val Iterator: Iterator[T] = elements.iterator
+    for (_ <- 0 until Math.min(Delta, CollSize)) Delegate.enqueue(Iterator.next)
+    for (_ <- 0 until Math.max(0, CollSize - Delta)) Delegate.enqueue(Iterator.next)
+    this
+  }
+
+  def contains(element: T): Boolean = Delegate.contains(checkNotNull(element, s"RQueue cannot contain null element"))
+  def length: Int = Delegate.length
+  def toArray: Array[T] = Delegate.toArray
 }
--- src/test/scala/RQueueSpec.scala
+++ src/test/scala/RQueueSpec.scala
@@ -1,3 +1,32 @@
-class RQueueSpec {
+import framework.RQueue
+import org.scalatest.flatspec.AnyFlatSpec
 
+import scala.language.postfixOps
+import scala.reflect.ClassTag
+
+case object RQueueSpec {
+  def VoidQueue[T: ClassTag]: RQueue[T] = new RQueue[T](0)
+  def EmptyQueue[T: ClassTag](i: Int): RQueue[T] = new RQueue[T](i)
+  def TemplateQueue: Int => RQueue[Int] = {i: Int => EmptyQueue[Int](i) ++ List.range(0, i).toArray}
+}
+
+class RQueueSpec extends AnyFlatSpec {
+  import RQueueSpec._
+  s"An RQueue" must s"throw an IAE when creating with negative size" in assertThrows[IllegalArgumentException]{new RQueue[Int](-1)}
+  it must s"throw a NPE when adding a null element" in assertThrows[NullPointerException]{val test: String = null; VoidQueue[String] + test}
+  it must s"throw a NPE when adding a null arrays" in assertThrows[NullPointerException]{val test: Array[String] = null; VoidQueue[String] ++ test}
+  it must s"be empty on creation" in assertResult(0)(new RQueue[Int](10) length)
+  it must s"never add elements with size 0" in assertResult(0)(VoidQueue[Int] + 1 length)
+  it must s"expand accordingly" in assertResult(1)(EmptyQueue[Int](1) + 1 length)
+  it must s"add an element at the end" in assertResult(1)(EmptyQueue[Int](1) + 1 toArray 0)
+  it must s"add elements at the end" in {
+    val Queue: Array[Int] = (EmptyQueue[Int](3) + 0 + 1 + 2).toArray
+    for (i <- 0 to 2) assertResult(i)(Queue(i))}
+  it must s"add arrays of elements at the end" in {
+    val Queue: Array[Int] = TemplateQueue(3).toArray
+    for (i <- 0 to 2) assertResult(i)(Queue(i))}
+  it must s"confirm when items are contained in the queue" in {
+    val Queue: RQueue[Int] = TemplateQueue(5)
+    for (i <- 0 to 4) assertResult(true)(Queue.contains(i))
+    for (i <- 5 to 10) assertResult(false)(Queue.contains(i))}
 }
