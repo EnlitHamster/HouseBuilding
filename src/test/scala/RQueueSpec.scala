e604415fb46c4e896bfd8f0cc3cff93d412ea2b8
Sandro Massa <sandro2398@hotmail.it>
2020-04-06 15:24:50 +0200

    Made the RQueue resizable

--- src/test/scala/RQueueSpec.scala
+++ src/test/scala/RQueueSpec.scala
@@ -13,11 +13,12 @@ case object RQueueSpec {
 class RQueueSpec extends AnyFlatSpec {
   import RQueueSpec._
   s"An RQueue" must s"throw an IAE when creating with negative size" in assertThrows[IllegalArgumentException]{new RQueue[Int](-1)}
-  it must s"throw a NPE when adding a null element" in assertThrows[NullPointerException]{val test: String = null; VoidQueue[String] + test}
-  it must s"throw a NPE when adding a null arrays" in assertThrows[NullPointerException]{val test: Array[String] = null; VoidQueue[String] ++ test}
+  it must s"throw a NPE when adding a null element" in assertThrows[NullPointerException]{VoidQueue[String] + null}
+  it must s"throw a NPE when adding a null arrays" in assertThrows[NullPointerException]{VoidQueue[String] ++ null}
   it must s"be empty on creation" in assertResult(0)(new RQueue[Int](10) length)
   it must s"never add elements with size 0" in assertResult(0)(VoidQueue[Int] + 1 length)
   it must s"expand accordingly" in assertResult(1)(EmptyQueue[Int](1) + 1 length)
+  it must s"expand accordingly with arrays" in assertResult(5)(EmptyQueue[Int](5) ++ List.range[Int](0, 10).toArray length)
   it must s"add an element at the end" in assertResult(1)(EmptyQueue[Int](1) + 1 toArray 0)
   it must s"add elements at the end" in {
     val Queue: Array[Int] = (EmptyQueue[Int](3) + 0 + 1 + 2).toArray
