e434edd4ddbf97df895cc41ed6ed81da7a832249
Sandro Massa <sandro2398@hotmail.it>
2020-04-06 15:24:50 +0200

    Made the RQueue resizable

--- src/main/scala/framework/RQueue.scala
+++ src/main/scala/framework/RQueue.scala
@@ -5,30 +5,34 @@ import Predicts._
 
 import scala.reflect.ClassTag
 
-class RQueue[T: ClassTag](val Size: Int) {
-  checkArg(Size >= 0, s"Size (%s) must >= 0", Size)
+class RQueue[T: ClassTag](var size: Int) {
+  checkArg(size >= 0, s"Size (%s) must >= 0", size)
   private val Delegate = mutable.Queue[T]()
 
   def +(element: T): RQueue[T] = {
     checkNotNull(element, s"Cannot add null element to RQueue")
-    if (Size == 0) return this
-    if (Delegate.length == Size) Delegate.dequeue
+    if (size == 0) return this
+    if (Delegate.length == size) Delegate.dequeue
     Delegate.enqueue(element)
     this
   }
 
   def ++(elements: Array[T]): RQueue[T] = {
     checkNotNull(elements, s"Cannot add null element to RQueue")
-    if (Size == 0) return this
-    val Delta: Int = Size - Delegate.length
-    val CollSize: Int = elements.length
-    val Iterator: Iterator[T] = elements.iterator
-    for (_ <- 0 until Math.min(Delta, CollSize)) Delegate.enqueue(Iterator.next)
-    for (_ <- 0 until Math.max(0, CollSize - Delta)) Delegate.enqueue(Iterator.next)
+    if (size == 0) return this
+    for (e <- elements) Delegate.enqueue(e)
+    drop()
     this
+  }
+
+  def resize(newSize: Int): Unit = {
+    checkArg(newSize >= 0, s"Size (%s) must >= 0", newSize)
+    size = newSize
+    drop()
   }
 
   def contains(element: T): Boolean = Delegate.contains(checkNotNull(element, s"RQueue cannot contain null element"))
   def length: Int = Delegate.length
   def toArray: Array[T] = Delegate.toArray
+  def drop(): Unit = while (Delegate.length > size) Delegate.dequeue
 }
